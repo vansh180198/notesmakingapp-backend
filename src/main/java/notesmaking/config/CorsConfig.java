54bb9a0e508ca904d3db009b5b3bf43264829d2b
vb180198 <vanshbhatia53@yahoo.com>
2025-01-06 15:46:39 +0530

    cors config updated

--- src/main/java/notesmaking/config/CorsConfig.java
+++ src/main/java/notesmaking/config/CorsConfig.java
@@ -12,7 +12,8 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         System.out.println("Configuring CORS mappings...");
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5174")
+                .allowedOrigins("https://notesmakingapp-frontend-murex.vercel.app", // Add your deployed frontend URL
+                        "http://localhost:5174")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
